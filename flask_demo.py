5050a68c2a1c0dd833d9514f3a4616dc17da6184
amolkhair25 <amol.khair25@gmail.com>
2023-11-27 23:25:15 +0530

    first Commit

--- flask_demo.py
+++ flask_demo.py
@@ -11,5 +11,10 @@ def test():
 def hello():
     return render_template("hello.html")
 
+@app.route("/status")
+def status():
+    return render_template("status.html")
+
+
 if __name__=="__main__":
     app.run(debug=True)
