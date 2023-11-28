8c45ef61d9b2097148e6965b66d978000fac9391
amolkhair25 <amol.khair25@gmail.com>
2023-11-28 22:26:31 +0530

    second commit

--- flask_demo.py
+++ flask_demo.py
@@ -1,20 +1,11 @@
-# Flask demo
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route("/")
-def test():
-    return render_template("test.html")
+def home():
+    return render_template("home.html")
 
-@app.route("/hello")#(@app.route decorator is used to define a route for your web application),(/hello ->URL route that will trigger the greet function)
-def hello():# hello function that will be called when a user visits the specified URL route
-    return render_template("hello.html")
-
-@app.route("/status")
-def status():
-    return render_template("status.html")
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
+    
