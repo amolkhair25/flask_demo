1e473348363d58e159852614fbe769af84e01669
amolkhair25 <amol.khair25@gmail.com>
2023-11-29 22:34:59 +0530

    "Third commit"

--- flask_demo.py
+++ flask_demo.py
@@ -7,6 +7,30 @@ app=Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/courses")
+def course():
+    return render_template("course.html")
+
+@app.route("/teacher")
+def teacher():
+    return render_template("teacher.html")
+
+@app.route("/blog_list")
+def blog_list():
+    return render_template("blog_list.html")
+
+@app.route("/blog_details")
+def blog_details():
+    return render_template("blog_details.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
     
