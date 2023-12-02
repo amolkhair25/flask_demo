b0b5046234686076e18a277482728b6b0edd0a55
amolkhair25 <amol.khair25@gmail.com>
2023-12-02 09:29:59 +0530

    CURD commit

--- flask_crud/app.py
+++ flask_crud/app.py
@@ -1,16 +1,11 @@
-#Install Flask-SQLAlchemy: pip install Flask Flask-SQLAlchemy
-#create app.py file
-#in your app.py file, import Flask and Flask-SQLAlchemy:
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, flash, url_for, redirect, render_template
 
-#Configure your Flask app:
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SECRET_KEY'] = "dev"
 app.app_context().push()
 
-#Create a model:
 db = SQLAlchemy(app)
 class students(db.Model):
 	id = db.Column('student_id', db.Integer, primary_key = True)
@@ -25,7 +20,7 @@ class students(db.Model):
 		self.addr = addr
 		self.pin = pin
 
-#Using the database in your routes:
+
 @app.route('/')
 def show_all():
 	return render_template('show_all.html', students = students.query.all() )
@@ -45,7 +40,31 @@ def new():
 			return redirect(url_for('show_all'))
 	return render_template('new.html')
 
-#Create the database:
+@app.route('/update/<int:student_id>', methods=['GET', 'POST'])
+def update_student(student_id):
+    student = students.query.get_or_404(student_id)
+
+    if request.method == 'POST':
+        student.name = request.form['name']
+        student.city = request.form['city']
+        student.addr = request.form['addr']
+        student.pin = request.form['pin']
+        db.session.commit()
+        return redirect(url_for('show_all'))
+
+    return render_template('update.html', student=student)
+
+@app.route('/delete/<int:student_id>', methods=['GET', 'POST'])
+def delete_student(student_id):
+    student = students.query.get(student_id)
+
+    if request.method == 'POST':
+        db.session.delete(student)
+        db.session.commit()
+        return redirect(url_for('show_all'))
+
+    return render_template('delete.html', student=student)
+
 if __name__ == '__main__':
    db.create_all()
    app.run(debug = True)
